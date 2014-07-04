9d3b91e4292de1b340f8ceefeb16623283e00bea
Ruben Jeronimo Fernandez <rubenjeronimofernandez@MacBook-Pro-de-Ruben.local>
2014-07-04 16:37:16 +0200

    Initial Commit

---
+++ eventosEuskadi/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  eventosEuskadi
+//
+//  Created by Ruben Jeronimo Fernandez on 04/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ eventosEuskadiTests/eventosEuskadiTests.m
@@ -0,0 +1,34 @@
+//
+//  eventosEuskadiTests.m
+//  eventosEuskadiTests
+//
+//  Created by Ruben Jeronimo Fernandez on 04/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface eventosEuskadiTests : XCTestCase
+
+@end
+
+@implementation eventosEuskadiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
